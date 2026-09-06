3f8db78310d22ae66b536042a6f5d6639233c277
Kiuliumov <miti.bania@gmail.com>
2026-09-06 10:57:27 +0300

    Remove unused import

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model, login, logout
 from django.middleware.csrf import get_token
 from django.urls import reverse
 from rest_framework import permissions, status
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
