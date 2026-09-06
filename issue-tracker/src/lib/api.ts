bb545a5407dd8704900bcdf8739546ec99197200
Kiuliumov <miti.bania@gmail.com>
2026-09-06 09:25:13 +0300

    Attach csrf in api

--- issue-tracker/src/lib/api.ts
+++ issue-tracker/src/lib/api.ts
@@ -1,8 +1,33 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
+});
+
+let csrfToken: string | null = null;
+
+export async function getCsrfToken() {
+  if (csrfToken) {
+    return csrfToken;
+  }
+
+  const response = await api.get("/accounts/csrf/");
+  csrfToken = response.data.csrfToken;
+
+  return csrfToken;
+}
+
+api.interceptors.request.use(async (config) => {
+  const method = config.method?.toUpperCase();
+
+  if (method && ["POST", "PUT", "PATCH", "DELETE"].includes(method)) {
+    const token = await getCsrfToken();
+
+    config.headers.set("X-CSRFToken", token);
+  }
+
+  return config;
 });
 
 export default api;
